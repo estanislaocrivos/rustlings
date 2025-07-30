bdf8922aab7958d8ba8902f87c4b9e6ac0cfb83e
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-07-30 14:19:17 -0300

    First commit.

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,12 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a: [i32; 100] = [0; 100];
+
+    let mut index: i32 = 0;
+    for element in a.iter() {
+        println!("a[{}] = {}", index, element);
+        index += 1;
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -2,7 +2,10 @@ fn main() {
     let cat = ("Furry McFurson", 3.5);
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
-    // let /* your pattern here */ = cat;
+    let name: &str = cat.0;
+    let age: f64 = cat.1;
+
+    // let (name, age) = cat; // Another way...
 
     println!("{name} is {age} years old");
 }
